3af9cc9da93e3a7ab7bf2f5781bba43d96f1571d
baspalinckx <bas.palinckx@hotmail.nl>
2018-08-02 16:09:04 +0200

    added form html need to implement in post html

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -8,6 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  use App\Entity\Post;
  use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\HttpFoundation\Request;
+ use Symfony\Component\Form\Extension\Core\Type\TextType;
+ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 //test commit 3
 
 class PostController extends Controller
@@ -21,7 +24,6 @@ class PostController extends Controller
            ->getRepository(Post::class)
            ->findAll();
 
-      // echo $posts
 
 
        if (!$posts) {
@@ -32,33 +34,41 @@ class PostController extends Controller
 
 
        return $this->render('post/index.html.twig', ['posts' => $posts]);
+
+
      }
 
-     /**
-      * @Route("/post/{id}", name="product_show")
-      */
-     public function show($id)
+     public function addPost(Request $request)
      {
-         $post = $this->getDoctrine()
-             ->getRepository(Post::class)
-             ->find($id);
+         if ($request->getMethod() == 'POST') {
+             // EntityManager
+             $em = $this->getDoctrine()->getEntityManager();
 
-         if (!$post) {
-             throw $this->createNotFoundException(
-                 'No product found for id '.$id
-             );
-         }
+             // New entity
+             $post = new Post();
 
+             // Build the form
+             $form = $this->createFormBuilder()
+             ->add('username', TextType::class)
+             ->add('message', TextType::class)
+             ->add('submit', SubmitType:: class)
+             ->getForm();
+             //
+             // // Populate
+             // $form->bindRequest($request);
+             //
+             // // Check
+             // if($form->isValid()) {
+             //     // Fill the entity
+             //     $post->setName($form['username']->getData());
+             //     $post->setMessage($form['message']->getData());
+             //     $em->persist($post);
+             //     $em->flush();
+             //   }
 
-         return new Response('Check out this great post: '.$post->getMessage());
-
-         // or render a template
-         // in the template, print things with {{ product.name }}
-         // return $this->render('product/show.html.twig', ['product' => $product]);
+           }
+           return $this->render('post/form.html.twig',array(
+                   'form' => $form->createView(),
+           ));
+       }
      }
-
-
-
-
-
- }

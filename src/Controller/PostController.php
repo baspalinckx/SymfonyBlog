bac82d465bce54a6c39015f864c4a2de02c23954
baspalinckx <bas.palinckx@hotmail.nl>
2018-08-07 13:05:22 +0200

    clean up more code

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,12 +6,12 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
- use App\Entity\Post;
- use Symfony\Component\HttpFoundation\Response;
- use Symfony\Component\HttpFoundation\Request;
- use Symfony\Component\Form\Extension\Core\Type\TextType;
- use Symfony\Component\Form\Extension\Core\Type\SubmitType;
- use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use App\Entity\Post;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 //test commit 3
 
@@ -44,55 +44,55 @@ class PostController extends Controller
          }
 
 
-         /**
-          * @Route("/post/add" , name="addpost" )
-          */
-
-         public function addPost(Request $request)
-         {
-
-                 // EntityManager
-                 $em = $this->getDoctrine()->getEntityManager();
-
-                 // New entity
-                 $post = new Post();
-
-                 // Build the form
-                 $form = $this->createFormBuilder()
-                 ->add('username', TextType::class)
-                 ->add('message', TextType::class)
-                 ->add('submit', SubmitType:: class)
-                 ->getForm();
-
-                 // Populate
-
-                 $form->handleRequest($request);
-                 // Check
-                 if($form->isSubmitted()) {
-                     // Fill the entity
-                     $post->setUsername($form['username']->getData());
-                     $post->setMessage($form['message']->getData());
-                     $post->setTime(date('d/m/Y'));
-                     $em->persist($post);
-                     $em->flush();
-
-                     return $this->redirect($this->generateUrl('post'));
-
-                   }
-
-                 return $this->render('form/index.html.twig',array(
-                   'form' => $form->createView(),
-                       ));
-           }
-
-
      /**
-     * @param Post $post
-     *
-     * @Route("post/{id}/delete", requirements={"id" = "\d+"}, name="deletepost")
-     * @return RedirectResponse
-     *
-     */
+      * @Route("/post/add" , name="addpost" )
+      */
+
+     public function addPost(Request $request)
+     {
+       // EntityManager
+       $em = $this->getDoctrine()->getEntityManager();
+
+       // New entity
+       $post = new Post();
+
+       // Build the form
+       $form = $this->createFormBuilder()
+       ->add('username', TextType::class)
+       ->add('message', TextType::class)
+       ->add('submit', SubmitType:: class)
+       ->getForm();
+
+       // Populate
+
+       $form->handleRequest($request);
+       // Check
+       if($form->isSubmitted()) {
+
+         // Fill the entity
+         $post->setUsername($form['username']->getData());
+         $post->setMessage($form['message']->getData());
+         $post->setTime(date('d/m/Y'));
+         $em->persist($post);
+         $em->flush();
+
+         return $this->redirect($this->generateUrl('post'));
+
+         }
+
+       return $this->render('form/index.html.twig',array(
+         'form' => $form->createView(),
+             ));
+       }
+
+
+   /**
+   * @param Post $post
+   *
+   * @Route("post/{id}/delete", requirements={"id" = "\d+"}, name="deletepost")
+   * @return RedirectResponse
+   *
+   */
 
     public function deletePost(Post $post){
 
@@ -104,16 +104,14 @@ class PostController extends Controller
         throw $this->createNotFoundException(
             'No product found for id '.$id
         );
+      }
 
+      $em->remove($toDeletePost);
+      $em->flush();
 
-    }
+      return $this->redirect($this->generateUrl('post'));
 
-    $em->remove($toDeletePost);
-    $em->flush();
-
-    return $this->redirect($this->generateUrl('post'));
-
-   }
+     }
 
    /**
    * @param Post $post
@@ -124,46 +122,46 @@ class PostController extends Controller
    */
 
     public function updatePost(Request $request)
- {
+    {
 
-        $id = $request->get('id');
+      $id = $request->get('id');
 
-         // EntityManager
-         $em = $this->getDoctrine()->getEntityManager();
+       // EntityManager
+       $em = $this->getDoctrine()->getEntityManager();
 
-         // New entity
-         $toUpdatePost = $em->getRepository(Post::class)->find($id);
+       // New entity
+       $toUpdatePost = $em->getRepository(Post::class)->find($id);
 
-         // Build the form
-         $form = $this->createFormBuilder()
-         ->add('username', TextType::class, array(
-               'label' => 'Username: ',
-               'data' => $toUpdatePost->getUsername()
-          ))
-         ->add('message', TextType::class,  array(
-               'label' => 'Message: ',
-               'data' => $toUpdatePost->getMessage()
-          ))
-         ->add('submit', SubmitType:: class)
-         ->getForm();
+       // Build the form
+       $form = $this->createFormBuilder()
+       ->add('username', TextType::class, array(
+             'label' => 'Username: ',
+             'data' => $toUpdatePost->getUsername()
+        ))
+       ->add('message', TextType::class,  array(
+             'label' => 'Message: ',
+             'data' => $toUpdatePost->getMessage()
+        ))
+       ->add('submit', SubmitType:: class)
+       ->getForm();
 
-         // Populate
+       // Populate
 
-         $form->handleRequest($request);
-         // Check
-         if($form->isSubmitted()) {
-             // Fill the entity
-             $toUpdatePost->setUsername($form['username']->getData());
-             $toUpdatePost->setMessage($form['message']->getData());
-             $em->persist($toUpdatePost);
-             $em->flush();
+       $form->handleRequest($request);
+       // Check
+       if($form->isSubmitted()) {
+           // Fill the entity
+           $toUpdatePost->setUsername($form['username']->getData());
+           $toUpdatePost->setMessage($form['message']->getData());
+           $em->persist($toUpdatePost);
+           $em->flush();
 
-             return $this->redirect($this->generateUrl('post'));
+           return $this->redirect($this->generateUrl('post'));
 
-           }
+         }
 
-         return $this->render('form/index.html.twig',array(
-           'form' => $form->createView(),
-               ));
+       return $this->render('form/index.html.twig',array(
+         'form' => $form->createView(),
+             ));
    }
  }

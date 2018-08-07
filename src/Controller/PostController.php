d767818037bcd265d5e05afdfd6c7d7eeac49bb8
baspalinckx <bas.palinckx@hotmail.nl>
2018-08-07 12:14:28 +0200

    added update function

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -45,28 +45,86 @@ class PostController extends Controller
      * @return RedirectResponse
      *
      */
-public function deletePost(Post $post){
+    public function deletePost(Post $post){
 
-  $em = $this->getDoctrine()->getManager();
+      $em = $this->getDoctrine()->getManager();
 
-  $toDeletePost = $em->getRepository(Post::class)->find($post);
+      $toDeletePost = $em->getRepository(Post::class)->find($post);
 
-  if (!$toDeletePost) {
-    throw $this->createNotFoundException(
-        'No product found for id '.$id
-    );
-
-
-}
-
-$em->remove($toDeletePost);
-$em->flush();
-
-return $this->redirect($this->generateUrl('post'));
+      if (!$toDeletePost) {
+        throw $this->createNotFoundException(
+            'No product found for id '.$id
+        );
 
 
+    }
+
+    $em->remove($toDeletePost);
+    $em->flush();
+
+    return $this->redirect($this->generateUrl('post'));
 
    }
 
+   /**
+   * @param Post $post
+   *
+   * @Route("post/{id}/update", requirements={"id" = "\d+"}, name="updatepost")
+   * @return RedirectResponse
+   *
+   */
 
+    public function addPost(Request $request)
+ {
+
+        $id = $request->get('id');
+
+         // EntityManager
+         $em = $this->getDoctrine()->getEntityManager();
+
+         // New entity
+         $toUpdatePost = $em->getRepository(Post::class)->find($id);
+
+         // Build the form
+         $form = $this->createFormBuilder()
+         ->add('username', TextType::class)
+         ->add('message', TextType::class)
+         ->add('submit', SubmitType:: class)
+         ->getForm();
+
+         // Populate
+
+         $form->handleRequest($request);
+         // Check
+         if($form->isSubmitted()) {
+             // Fill the entity
+             $toUpdatePost->setUsername($form['username']->getData());
+             $toUpdatePost->setMessage($form['message']->getData());
+             $em->persist($toUpdatePost);
+             $em->flush();
+
+             return $this->redirect($this->generateUrl('post'));
+
+           }
+
+         // $data = json_decode($request->getContent());
+         //
+         // if (isset($data->username)) {
+         //     $post->setUsername($data->username);
+         // }
+         //
+         // if (isset($data->message)) {
+         //     $entity->setMessage($data->message);
+         // }
+         //
+         //
+         // $em->persist($post);
+         // $em->flush();
+
+
+
+         return $this->render('form/index.html.twig',array(
+           'form' => $form->createView(),
+               ));
+   }
  }
